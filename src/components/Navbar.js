6ef96e8bfa168978d1591603d9f1340a2860407d
Miro Rauhala <miro@rauhala.dev>
2021-12-05 16:14:40 +0200

    Reindent

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,32 @@
-import {NavLink} from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-    return (
-        <nav className="border-b p-4 flex">
+  return (
+    <nav className="border-b p-4 flex">
+      <span className="font-bold pr-4">App</span>
 
-            <span className="font-bold pr-4">App</span>
-            
-            <ul className="flex">
-                <li>
-                    <NavLink
-                        className={({ isActive }) => {return isActive ? 'font-bold px-2' : ' px-2'}}
-                        to="/">
-                        Etusivu
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        className={({ isActive }) => {return isActive ? 'font-bold px-2' : ' px-2'}}
-                        to="/about">
-                        Tietoa
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+      <ul className="flex">
+        <li>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? "font-bold px-2" : " px-2";
+            }}
+            to="/"
+          >
+            Etusivu
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => {
+              return isActive ? "font-bold px-2" : " px-2";
+            }}
+            to="/about"
+          >
+            Tietoa
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
