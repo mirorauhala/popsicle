283971304d85f8897ece6532a8b489aa964a95d9
Miro Rauhala <miro@rauhala.dev>
2021-12-05 16:12:48 +0200

    cs

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,5 +1,5 @@
 export default function Task(props) {
-    return (
-        <div className="p-4 bg-gray-100 w-64 rounded-xl">{props.task.body}</div>
-    )
+  return (
+    <div className="p-4 bg-gray-100 w-64 rounded-xl">{props.task.body}</div>
+  );
 }
