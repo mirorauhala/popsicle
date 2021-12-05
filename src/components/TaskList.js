d1570e34d3acec04510b46fa4477e785f176728f
Miro Rauhala <miro@rauhala.dev>
2021-12-05 16:12:40 +0200

    Fix context

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,24 +1,25 @@
-import Task from './Task'
-import NewTask from './NewTask'
-import { TaskContext } from '../context/TasksContext'
-import { useContext } from 'react'
+import Task from "./Task";
+import NewTask from "./NewTask";
+import { TaskContext } from "../Board";
+import { useContext } from "react";
 
 export default function TaskList(props) {
-    const [tasks, addTask] = useContext(TaskContext)
+  const tasks = useContext(TaskContext);
 
-    const filteredTasks = () => {
-        console.log(tasks)
-        // const filteredTasks = tasks.filter(task => task.boardId === props.list.id)
-        // return filteredTasks.map(task => <Task key={task.id} task={task} />)
-    }
+  const filteredTasks = () => {
+    const filteredTasks = tasks.filter(
+      (task) => task.boardId === props.list.id,
+    );
+    return filteredTasks.map((task) => <Task key={task.id} task={task} />);
+  };
 
-    return (
-        <div>
-            <h5 className="my-3">{props.list.name}</h5>
-            <div className="flex flex-col gap-3">
-                <NewTask list={props.list} />
-                {filteredTasks()}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h5 className="my-3">{props.list.name}</h5>
+      <div className="flex flex-col gap-3">
+        <NewTask list={props.list} />
+        {filteredTasks()}
+      </div>
+    </div>
+  );
 }
