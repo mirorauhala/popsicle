eee98ed8d5124a187fc2e991293ad6b20aa0465d
Miro Rauhala <miro@rauhala.dev>
2021-12-05 16:12:29 +0200

    Remove context for now

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,25 +1,19 @@
-import { useContext } from 'react'
-import { TaskContext } from '../context/TasksContext'
-import {ReactComponent as Plus} from '../icons/plus.svg'
+import { ReactComponent as Plus } from "../icons/plus.svg";
 
 export default function NewTask(props) {
-    const tasks = useContext(TaskContext)
+  return (
+    <form className="flex" onSubmit={(e) => console.log("submit")}>
+      <input type="hidden" name="board" value={props.list.id} />
+      <input
+        className="w-full px-3 py-1 border-2 border-gray-100 rounded-l-xl"
+        type="text"
+        placeholder="New task"
+        required
+      />
 
-    return (
-        <form className="flex" onSubmit={""}>
-            <input type="hidden" name="board" value={props.list.id} />
-            <input
-                className="w-full px-3 py-1 border-2 border-gray-100 rounded-l-xl"
-                type="text"
-                placeholder="New task"
-                required />
-
-                <button
-                    type="submit"
-                    className="p-2 bg-gray-100 rounded-r-xl"
-                >
-                    <Plus />
-                </button>
-        </form>
-    )
+      <button type="submit" className="p-2 bg-gray-100 rounded-r-xl">
+        <Plus />
+      </button>
+    </form>
+  );
 }
